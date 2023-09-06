0cadde6509a11d4202d47f94cc75cba9b29d8d86
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-09-06 08:57:01 +0200

    aflevering 2.0

---
+++ Backend/script.js
@@ -0,0 +1,101 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs/promises";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+async function getArtists() {
+  const artistList = await fs.readFile("artistData.json");
+  return JSON.parse(artistList);
+  // console.log(artists);
+
+  // return artists;
+}
+
+// async function makeNewArtist(artists) {
+//   const json = JSON.stringify(artists);
+//   await fs.writeFile("artistData.json", json);
+// }
+
+app.listen(port, () => {
+  console.log(`noget sjovt skete... server kører på http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("vi får respons! 🎉👌");
+});
+
+app.get("/artists", async (req, res) => {
+  const artists = await getArtists();
+  //   res.send("vi får artister! 🎉");
+  //   const artistList = await fs.readFile("artistData.json");
+  //   const artists = JSON.parse(artistList);
+  getArtists(artists);
+  res.json(artists);
+  // console.log(artists);
+});
+
+app.get("/artists/:id", async (req, res) => {
+  // getArtists(artists);
+  const artists = await getArtists();
+  console.log(req.params);
+
+  //   const artistList = await fs.readFile("artistData.json");
+  //   const artists = JSON.parse(artistList);
+
+  const id = Number(req.params.id);
+  const findArtist = artists.find((artist) => artist.id === id);
+  res.json(findArtist);
+});
+
+app.post("/artists", async (req, res) => {
+  console.log(req.body);
+  const newArtist = req.body;
+  newArtist.id = new Date().getTime();
+  console.log(newArtist);
+
+  const artists = await getArtists();
+  artists.push(newArtist);
+  const json = JSON.stringify(artists);
+  await fs.writeFile("artistData.json", json);
+  res.json(artists);
+});
+
+app.put("/artists/:id", async (req, res) => {
+  console.log("vi putter");
+  const id = req.params.id;
+  console.log(id);
+
+  const artists = await getArtists();
+  // const artists = JSON.parse(data);
+
+  const artistToUpdate = artists.find((artist) => Number(artist.id) === Number(id));
+  const body = req.body;
+  console.log(body);
+  artistToUpdate.name = body.name;
+  artistToUpdate.birthdate = body.birthdate;
+  artistToUpdate.activeSince = body.activeSince;
+  artistToUpdate.genres = body.genres;
+  artistToUpdate.labels = body.labels;
+  artistToUpdate.website = body.website;
+  artistToUpdate.image = body.image;
+  artistToUpdate.shortDescription = body.shortDescription;
+
+  fs.writeFile("artistData.json", JSON.stringify(artists));
+  res.json(artists);
+});
+
+app.delete("/artists/:id", async (req, res) => {
+  const id = Number(req.params.id);
+
+  const artists = await getArtists();
+
+  const NewArtistList = artists.filter((artist) => artist.id !== id);
+  fs.writeFile("artistData.json", JSON.stringify(NewArtistList));
+
+  res.json(artists);
+});
---
+++ Frontend/script.js
@@ -0,0 +1,157 @@
+"use strict";
+
+window.addEventListener("load", start);
+const endpoint = "http://localhost:3000";
+let selectedArtist;
+
+function start() {
+  console.log("we have connection to js 👌🙌");
+
+  document.querySelector("#btn-create-artist").addEventListener("click", () => document.querySelector("#dialog-create-artist").showModal());
+
+  document.querySelector("#form-create").addEventListener("submit", createArtist);
+  document.querySelector("#form-update").addEventListener("submit", submitUpdatedArtist);
+
+  updateArtistpage();
+  console.log("START FUNC");
+}
+
+async function updateArtistpage() {
+  const artists = await getArtistsFromBackend();
+  showArtistsAll(artists);
+}
+
+async function getArtistsFromBackend() {
+  const response = await fetch(`${endpoint}/artists`);
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
+
+function showArtistsAll(array) {
+  document.querySelector("#artist-table-body").innerHTML = "";
+
+  for (const artist of array) {
+    showArtist(artist);
+  }
+}
+
+function showArtist(artist) {
+  const html = /* HTML */ `
+    <tr class="artist-item">
+      <td>
+        <button class="btn-update-artist" class="buttonAni">Update</button>
+        <button class="btn-delete-artist" class="buttonAni">Delete</button>
+      </td>
+      <td>${artist.name}</td>
+      <td>${artist.birthdate}</td>
+      <td>${artist.activeSince}</td>
+      <td>${artist.genres}</td>
+      <td>${artist.labels}</td>
+      <td>${artist.website}</td>
+      <td><img src=${artist.image} /></td>
+      <td>${artist.shortDescription}</td>
+      <td>
+        <input type="checkbox" id="favoriteArtist" name="favoriteArtist" value="true" />
+        <label for="favoriteArtist">Favorite</label><br />
+      </td>
+    </tr>
+  `;
+
+  document.querySelector("#artist-table-body").insertAdjacentHTML("beforeend", html);
+
+  document.querySelector("#artist-table-body tr:last-child .btn-update-artist").addEventListener("click", () => selectedToUpdate(artist));
+  document.querySelector("#artist-table-body tr:last-child .btn-delete-artist").addEventListener("click", () => deleteArtist(artist.id));
+}
+
+async function createArtist(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const newArtist = {
+    name: form.name.value,
+    birthdate: form.birthdate.value,
+    activeSince: form.activeSince.value,
+    genres: form.genres.value,
+    labels: form.labels.value,
+    website: form.website.value,
+    image: form.image.value,
+    shortDescription: form.shortDescription.value
+  };
+
+  // JSONify the new artist
+  const artistAsJson = JSON.stringify(newArtist);
+  const response = await fetch(`${endpoint}/artists`, {
+    method: "POST",
+    body: artistAsJson,
+    headers: { "Content-Type": "application/json" }
+  });
+
+  if (response.ok) {
+    // if success, update the grid
+    updateArtistpage();
+    // and scroll to top
+    // scrollToTop();
+    scrollIntoView({ behavior: "smooth" });
+  } else {
+    console.error("Something went wrong with posting a new artist Lise");
+  }
+}
+
+function selectedToUpdate(artist) {
+  selectedArtist = artist;
+  console.log("my selected artist  ", selectedArtist);
+
+  const form = document.querySelector("#form-update");
+  form.name.value = artist.name;
+  form.birthdate.value = artist.birthdate;
+  form.activeSince.value = artist.activeSince;
+  form.genres.value = artist.genres;
+  form.labels.value = artist.labels;
+  form.website.value = artist.website;
+  form.image.value = artist.image;
+  form.shortDescription.value = artist.shortDescription;
+
+  document.querySelector("#dialog-update-artist").showModal();
+}
+
+async function submitUpdatedArtist(event) {
+  event.preventDefault();
+  console.log("my updated artist  ", selectedArtist.id);
+
+  const form = event.target;
+  const updatedArtist = {
+    name: form.name.value,
+    birthdate: form.birthdate.value,
+    activeSince: form.activeSince.value,
+    genres: form.genres.value,
+    labels: form.labels.value,
+    website: form.website.value,
+    image: form.image.value,
+    shortDescription: form.shortDescription.value
+  };
+
+  // JSONify the updated artist
+  const artistAsJson = JSON.stringify(updatedArtist);
+  const response = await fetch(`${endpoint}/artists/${selectedArtist.id}`, {
+    method: "PUT",
+    body: artistAsJson,
+    headers: { "Content-Type": "application/json" }
+  });
+  if (response.ok) {
+    updateArtistpage();
+    scrollToTop({ behavior: "smooth" });
+  }
+}
+
+async function deleteArtist(id) {
+  console.log(id);
+  const response = await fetch(`${endpoint}/artists/${id}`, {
+    method: "DELETE"
+  });
+
+  if (response.ok) {
+    updateArtistpage();
+    scrollToTop({ behavior: "smooth" });
+  }
+}
